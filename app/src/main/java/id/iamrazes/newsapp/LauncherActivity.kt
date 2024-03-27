df9f6a84e0c0e049d89fe261917984eacdbac21f
Muhammad Dagistan Silawane (Razes) <71701718+iamrazes@users.noreply.github.com>
2024-03-27 18:24:14 +0700

    Initial commit

---
+++ app/src/main/java/id/iamrazes/newsapp/LauncherActivity.kt
@@ -0,0 +1,56 @@
+package id.iamrazes.newsapp
+
+import android.content.Context
+import android.content.DialogInterface
+import android.content.Intent
+import android.net.ConnectivityManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AlertDialog
+
+@Suppress("DEPRECATION")
+class LauncherActivity : AppCompatActivity() {
+
+    private val SPLASH_DELAY_MS = 3000L
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_launcher)
+        supportActionBar?.hide()
+
+        val handler = Handler(Looper.getMainLooper())
+        handler.postDelayed({
+            initialCheck()
+        }, SPLASH_DELAY_MS)
+    }
+
+    private fun initialCheck() {
+        if (!isFinishing && isInternetAvailable()) {
+            startActivity(Intent(this@LauncherActivity, NewsActivity::class.java))
+            finish()
+        } else if (!isFinishing) {
+            showBadConnectionDialog()
+        }
+    }
+
+    private fun isInternetAvailable(): Boolean {
+        val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val networkInfo = connectivityManager.activeNetworkInfo
+        return networkInfo != null && networkInfo.isConnected
+    }
+
+    private fun showBadConnectionDialog() {
+        AlertDialog.Builder(this)
+            .setTitle("Bad Connection, Try Again!")
+            .setMessage("Check Your Internet Connectivity.")
+            .setPositiveButton("OK") { dialogInterface: DialogInterface, _: Int ->
+                dialogInterface.dismiss()
+                finishAffinity()
+            }
+            .setCancelable(false)
+            .show()
+    }
+}
